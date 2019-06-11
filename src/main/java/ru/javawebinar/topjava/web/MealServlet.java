caa071e11830a7856c58af69ac91e351a3b9df6f
ShadySentry <eurohawk@gmail.com>
2019-06-11 16:50:06 +0300

    jspl with context error

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.InitializeMetadataUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,7 +20,13 @@ public class MealServlet extends HttpServlet {
         log.debug("redirected to meals");
 
         try{
-            req.getRequestDispatcher("/meals.jsp").forward(req,resp);
+
+//            getServletContext().setAttribute("singleMeal", getSingleMeal());
+            getServletContext().setInitParameter("singleMeal",getSingleMeal().toString());
+            getServletContext().getRequestDispatcher("/meals.jsp").forward(req, resp);
+
+//            req.getRequestDispatcher("/meals.jsp").forward(req,resp);
+
 //            resp.sendRedirect("meals.jsp");
         }
         catch (Exception ex){
@@ -26,5 +34,9 @@ public class MealServlet extends HttpServlet {
             throw ex;
         }
 //        super.doGet(req, resp);
+    }
+
+    private Meal getSingleMeal(){
+        return InitializeMetadataUtil.getMeals().get(0);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -7,5 +7,17 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<%--<h1>Single meal ${singleMeal}</h1>--%>
+<h1><%
+    String singleMeal = request.getParameter("singleMeal");
+    if(singleMeal==null||singleMeal.length()==0){
+        %>
+    Single meal
+    <% }
+    else {
+        %>
+    Single meal <%=singleMeal%>
+    <%}
+%></h1>
 </body>
 </html>
