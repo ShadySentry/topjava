bf606a63ea596ddaee8062e3463b8d166066d54b
ShadySentry <eurohawk@gmail.com>
2019-06-29 14:08:11 +0300

    preparing to HW04

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
@@ -37,9 +36,7 @@ public class MealService {
         return getBetweenDateTimes(adjustStartDateTime(startDate), adjustEndDateTime(endDate), userId);
     }
 
-    private List<Meal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        Assert.notNull(startDateTime, "startDateTime must not be null");
-        Assert.notNull(endDateTime, "endDateTime  must not be null");
+    public List<Meal> getBetweenDateTimes(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
         return repository.getBetween(startDateTime, endDateTime, userId);
     }
 
@@ -48,12 +45,10 @@ public class MealService {
     }
 
     public void update(Meal meal, int userId) {
-        Assert.notNull(meal, "meal must not be null");
         checkNotFoundWithId(repository.save(meal, userId), meal.getId());
     }
 
     public Meal create(Meal meal, int userId) {
-        Assert.notNull(meal, "meal must not be null");
         return repository.save(meal, userId);
     }
 }
