3f20a93b637890a530036e3653222506ef40a90a
ShadySentry <eurohawk@gmail.com>
2019-06-29 21:57:29 +0300

    jdbc driver loading error

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepository.java
@@ -8,6 +8,7 @@ import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -47,11 +48,11 @@ public class JpaUserRepository implements UserRepository {
     public boolean delete(int id) {
 
 /*      User ref = em.getReference(User.class, id);
-        em.remove(ref);
+        em.remove(ref);*/
 
-        Query query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
-        return query.setParameter("id", id).executeUpdate() != 0;
-*/
+//        Query query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
+//        return query.setParameter("id", id).executeUpdate() != 0;
+
         return em.createNamedQuery(User.DELETE)
                 .setParameter("id", id)
                 .executeUpdate() != 0;
