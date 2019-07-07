a2907c63c413061d396236caaaec290219c1f08d
ShadySentry <eurohawk@gmail.com>
2019-07-07 21:29:11 +0300

    DataJpaMealRepository implemented

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,38 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    // null if updated meal do not belong to userId
+    @Transactional
+    @Modifying
+    Meal save(Meal meal, int userId);
+
+    // false if meal do not belong to userId
+    @Transactional
+    @Modifying
+    @Query(name = Meal.DELETE)
+    int delete(@Param("id") int mealId, @Param("userId") int userId);
+
+    // null if meal do not belong to userId
+//    @Query("select m from Meal m where m.id=:id and m.user.id=:userId")
+    Meal get(int id, int userId);
+
+    // ORDERED dateTime desc
+    List<Meal> getAll(int userId);
+
+    // ORDERED dateTime desc
+//    (name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m " +
+//            "WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
+    @Query(name = Meal.GET_BETWEEN)
+    List<Meal> getBetween(@Param("startDate")LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,41 +1,74 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
+    private static final Sort SORT_DATE = new Sort(Sort.Direction.DESC, "date_time");
+
     @Autowired
     private CrudMealRepository crudRepository;
 
+//    @PersistenceContext
+//    private EntityManager em;
+    @Autowired
+    private DataJpaUserRepository userRepository;
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        if(meal.isNew()) {
+            meal.setUser(userRepository.get(userId));
+        }
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        Meal storedMeal = crudRepository.findById(id).orElse(null);
+        if (storedMeal != null) {
+            if (storedMeal.getUser().getId() != userId) {
+                throw new NotFoundException("Incorrect userId for meal");
+            }
+        }
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal storedMeal = crudRepository.findById(id).orElse(null);
+        if (storedMeal != null && storedMeal.getUser() != null) {
+            if (storedMeal.getUser().getId() != userId) {
+                throw new NotFoundException("Incorrect userId for meal");
+            }
+        }
+        return storedMeal;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        List<Meal> storedMeals = crudRepository.findAll(SORT_DATE);
+        return storedMeals.stream().filter(m -> m.getUser().getId() == userId).collect(Collectors.toList());
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.getBetween(startDate, endDate, userId);
     }
 }
