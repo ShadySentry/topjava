18fac9cfaad4d522b05306c3226a92261d794732
ShadySentry <eurohawk@gmail.com>
2019-07-11 22:38:54 +0300

    hsqldb fix in pom.xml

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcPostgresMealRepository.java
@@ -0,0 +1,65 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public class JdbcPostgresMealRepository extends JdbcMealRepositoryAbstract {
+
+    @Autowired
+    public JdbcPostgresMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+//    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+////
+//    private final JdbcTemplate jdbcTemplate;
+////
+//    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+////
+//    private final SimpleJdbcInsert insertMeal;
+//ToDo:Разделить JdbcMealRepository для HSQLDB (она не умеет работать с Java8 Time API) и Postgres через @Profile (для Postgres оставить LocalDateTime).
+//    https://refactoring.guru/ru/design-patterns/template-method
+
+//    @Autowired
+//    public JdbcPostgresMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+//        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+//                .withTableName("meals")
+//                .usingGeneratedKeyColumns("id");
+//
+//        this.jdbcTemplate = jdbcTemplate;
+//        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+//    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        List<Meal> meals = jdbcTemplate.query(
+                "SELECT * FROM meals WHERE id = ? AND user_id = ?", ROW_MAPPER, id, userId);
+        return DataAccessUtils.singleResult(meals);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
+    }
+
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, startDate, endDate);
+    }
+}
